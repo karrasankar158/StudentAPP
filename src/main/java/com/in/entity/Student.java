b560d1186415922f7cbd7c22e9d6cb98b80ff342
karrasankar158 <karrasankar09@gmail.com>
2024-08-25 20:30:20 +0530

    SpringFox Swagger-UI Implementation

--- src/main/java/com/in/entity/Student.java
+++ src/main/java/com/in/entity/Student.java
@@ -1,7 +1,10 @@
 package com.in.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Id;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
